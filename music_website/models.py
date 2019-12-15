5b012ace51a3613ddf2da48acae67c718693e477
Michael Deutsch <michaeldeutsch1@gmail.com>
2019-12-15 10:22:05 +0200

    trying to divide responsibilities

--- music_website/models.py
+++ music_website/models.py
@@ -4,20 +4,27 @@ from music_website import db
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(15), unique=True)
-    email = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(80))
+    username = db.Column(db.String(16), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password_hashed = db.Column(db.String(80), nullable=False)
     first_name = db.Column(db.String(32))
     last_name = db.Column(db.String(32))
 
 
-
 class Music(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey(user.id))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    path = db.Column(db.String(256))
 
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    music_id = Column(db.Integer, ForeignKey('music.id'))
-    author_id = Column(db.Integer, ForeignKey('user.id'))
+    music_id = db.Column(db.Integer, db.ForeignKey('music.id'))
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#
+# class Tag(db.Model):
+#     pass
+#
+# project = db.Table(
+#
+# )
---
+++ music_website/repositories.py
@@ -0,0 +1,6 @@
+from music_website import db
+from music_website.models import User
+
+class UserRepository:
+    def register(self, email):
+        pass
