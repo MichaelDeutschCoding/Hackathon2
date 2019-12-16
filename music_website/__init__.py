46fc7e122cba995b653246e38ef83ed009770594
yosefbrowncode <57085910+yosefbrowncode@users.noreply.github.com>
2019-12-16 09:38:02 +0200

    frontend in separate folder

--- music_website/__init__.py
+++ music_website/__init__.py
@@ -1,11 +1,14 @@
 import random
-from flask import Flask
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from music_website.auth import login_manager
 from music_website.database import db, migrate
 
 
 app = Flask(__name__)
+
+
+
 
 app.config['SECRET_KEY'] = random._urandom(56)
 app.config['DEBUG'] = True
